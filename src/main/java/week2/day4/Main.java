e08e986fcade2ffb351f76b3c0685ca75d8719f6
Maudit00 <davide9311@gmail.com>
2024-01-18 18:47:39 +0100

    esercizio 5 fatto

--- src/main/java/week2/day4/Main.java
+++ src/main/java/week2/day4/Main.java
@@ -1,5 +1,8 @@
 package week2.day4;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -7,8 +10,8 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        Product p1 = new Product(1, "Harry Potter", "Books", 110);
-        Product p2= new Product(2, "Il Signore degli anelli ", "Books", 200);
+        Product p1 = new Product(1, "Harry Potter", "Books", 200.3);
+        Product p2= new Product(2, "Il Signore degli anelli ", "Books", 200.10);
         Product p3 = new Product(3, "Tre Metri sopra il cielo", "Books", 10);
         Product p4 = new Product(4, "Paperella di gomma", "Baby", 10);
         Product p5 = new Product(5, "Coltellino Svizzero", "Boys", 30);
@@ -29,18 +32,46 @@ public class Main {
         List<Order> orders = List.of(o1,o2,o3,o4,o5);
         List<Product> products = List.of(p1,p2,p3,p4,p5,p6,p7);
 
-        Map<Customer, List<Order> > orderByCustomer = orders.stream().collect(Collectors.groupingBy(Order::getCustomer));
-        Map<Customer, Double> totalByCustomer = orders.stream().collect(Collectors.groupingBy(Order::getCustomer, Collectors.summingDouble(order -> order.getProducts().stream().mapToDouble(Product::getPrice).sum())));
-        List<Product> expensiveProducts = products.stream().sorted((pro1,pro2) -> (int) (pro2.getPrice() - pro1.getPrice())).limit(3).toList();
-        Optional<Product> mostExpensive = products.stream().max(Comparator.comparingDouble(Product::getPrice));
+        Map<Customer, List<Order> > orderByCustomer = orders.stream()
+                .collect(Collectors.groupingBy(Order::getCustomer));
+
+        Map<Customer, Double> totalByCustomer = orders.stream()
+                .collect(Collectors.groupingBy(Order::getCustomer, Collectors.summingDouble(order -> order
+                        .getProducts()
+                        .stream()
+                        .mapToDouble(Product::getPrice)
+                        .sum())));
+
+        List<Product> expensiveProducts = products
+                .stream()
+                .sorted((pro1,pro2) -> (int) (pro2.getPrice() - pro1.getPrice()))
+                .limit(3)
+                .toList();
+
+        List<Product> expensiveProducts2 = products
+                .stream()
+                .sorted(Comparator.comparingDouble(Product::getPrice).reversed())
+                .limit(3)
+                .toList();
+
+
+
+        Product mostExpensive = products.stream().max(Comparator.comparingDouble(Product::getPrice)).get();
+
         Double averageCost = orders.stream().collect(Collectors.averagingDouble(order -> order.getProducts().stream().mapToDouble(Product::getPrice).sum()));
+        Map<String, Double> costByCat = products.stream()
+                .collect(Collectors.groupingBy(Product::getCategory, Collectors.summingDouble(Product::getPrice)));
 
         System.out.println(orderByCustomer);
         System.out.println(totalByCustomer);
         System.out.println("Prodotto più costoso " + mostExpensive);
         System.out.println("Lista dei più costosi");
         System.out.println(expensiveProducts);
+        System.out.println("Lista dei più costosi");
+        System.out.println(expensiveProducts2);
         System.out.println(averageCost);
+        System.out.println("Costo per categoria");
+        System.out.println(costByCat);
     }
 
 }
